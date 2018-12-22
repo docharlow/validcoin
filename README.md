0b0e2cea02c3c7e0194e18633a0dd9670d526e7d
Zumy Developer Team <dev@zumy.co>
2018-12-22 10:54:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+ValidCoin development tree
+
+ValidCoin is a scrypt PoS-based cryptocurrency.
+
+Coin Spec
+===========================
+
+Algo: 	scrypt
+
+
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ValidCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ValidCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
